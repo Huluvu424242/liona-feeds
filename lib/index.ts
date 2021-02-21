f279579bce3511ee967e636b6854c637bf8faad5
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-02-21 19:47:18 +0100

    [feature/projectsetup] jxjs converted

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,37 @@
-import * as jxjs from 'rxjs'
+import FeedMe, {Feed} from "feedme";
+import {from, Observable} from "rxjs";
+
 /**
  * @Method: Returns the plural form of any noun.
  * @Param {string}
  * @Return {string}
  */
-export function getFeed (url: string) : string {
-    return "not implemented yet";
+export function getFeed(queryUrl: string): Observable<Feed> {
+    return from(loadFeedData(queryUrl));
+}
+
+function loadFeedData(url: string): Promise<Feed> {
+    return new Promise<Feed>((resolve, reject) => {
+        fetch(url).then((response: Response) => {
+            if (response.status != 200) {
+                const error = new Error(`status code ${response.status}`);
+                console.error(error);
+                reject(error);
+            } else {
+                let parser = new FeedMe(true);
+                parser.on('finish', () => {
+                    try {
+                        const data: Feed = parser.done() as Feed;
+                        resolve(data);
+                    } catch (ex) {
+                        // expect to failed if no body
+                        console.warn("Error during read data of response " + ex);
+                        reject(ex);
+                    }
+
+                });
+                response.text().then((txt) => parser.end(txt));
+            }
+        });
+    });
 }
