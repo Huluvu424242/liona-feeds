d54e66b6b29a8c207b36484bcc8453873061e720
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-02-21 16:12:20 +0100

    [feature/projectsetup] project setup for nodejs and typescript erstellt

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+import * as jxjs from 'rxjs'
+/**
+ * @Method: Returns the plural form of any noun.
+ * @Param {string}
+ * @Return {string}
+ */
+export function getFeed (url: string) : string {
+    return "not implemented yet";
+}
