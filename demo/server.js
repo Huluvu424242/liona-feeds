c6249531f383ad205f17c44eace6c8eccd35d999
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-02-28 20:06:22 +0100

    [feature/projectsetup] server.js ready

--- demo/server.js
+++ demo/server.js
@@ -1,18 +1,16 @@
-import express from 'express'
-import getFeed from '../dist/cjs/index'
+const express = require('express');
+const feeder = require('../dist/cjs');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-// const init = ()=> {
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => res.send(getNewsFeed()))
     .get('/feed', (req, res) => res.send(getNewsFeed()))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 const getNewsFeed = () => {
-    return getFeed("https://www.zdf.de/rss/zdf/nachrichten").toPromise();
+    return feeder.getFeed("https://www.zdf.de/rss/zdf/nachrichten").toPromise();
 };
-
-// }
-
-// module.exports =  {init};
