8965ce94b66b5ba7a9048b5cdd814ba34d097868
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-03-08 21:49:44 +0100

    [feature/#3-uuid] refactoring logger separated

--- src/feeder.ts
+++ src/feeder.ts
@@ -3,6 +3,7 @@ import {EMPTY, from, Observable, Subscription, timer} from "rxjs";
 import * as objectHash from "object-hash";
 import axios, {AxiosResponse} from "axios";
 import {catchError, switchMap, tap} from "rxjs/operators";
+import {Logger} from "./logger";
 
 interface UrlMetaData {
     url: string;
@@ -14,37 +15,23 @@ interface UrlMetaData {
 
 class Feeder {
 
-    protected isLogActive = true;
+    protected LOG:Logger = new Logger();
 
     protected feedUrls: Map<string, UrlMetaData> = new Map();
 
-    protected logError(error: Error) {
-        if (console) {
-            console.error(error);
-        }
-    }
-
-    protected logDebug(message: string) {
-        if (this.isLogActive && console) console.debug(message);
-    }
-
-    protected logInfo(message: string) {
-        if (this.isLogActive && console) console.info(message);
-    }
-
     protected logMetadata(titel: string, feedData: UrlMetaData) {
-        this.logDebug(titel);
-        this.logDebug("Url:" + feedData.url);
-        this.logDebug("Period:" + feedData.period);
-        this.logDebug("Statistik:" + feedData.withStatistic);
+        this.LOG.logDebug(titel);
+        this.LOG.logDebug("Url:" + feedData.url);
+        this.LOG.logDebug("Period:" + feedData.period);
+        this.LOG.logDebug("Statistik:" + feedData.withStatistic);
     }
 
     public getFeedData = (url: string, period: number, withStatistic: boolean): Feed => {
-        this.logInfo("Eingehende Anfrage für " + url + " mit period: " + period + " und Statistik " + withStatistic);
+       this.LOG.logInfo("Eingehende Anfrage für " + url + " mit period: " + period + " und Statistik " + withStatistic);
         const key = objectHash.sha1(url);
         if (this.feedUrls.has(key)) {
             const feedData: UrlMetaData = this.feedUrls.get(key) as UrlMetaData;
-            this.logMetadata("Metadaten Alt", feedData);
+           this.logMetadata("Metadaten Alt", feedData);
             // Wechsel Statistik schreiben
             if (withStatistic !== feedData.withStatistic) {
                 feedData.withStatistic = withStatistic;
@@ -55,7 +42,7 @@ class Feeder {
                 feedData.subscription.unsubscribe();
                 feedData.subscription = this.getNewsFeed(url, key, period, withStatistic);
             }
-            this.logMetadata("Metadaten Neu", feedData);
+           this.logMetadata("Metadaten Neu", feedData);
             return feedData?.data;
         } else {
             const feedData: UrlMetaData = {
@@ -65,14 +52,14 @@ class Feeder {
                 data: {} as Feed,
                 subscription: this.getNewsFeed(url, key, period, withStatistic)
             };
-            this.logMetadata("Erstelle Metadaten", feedData);
+           this.logMetadata("Erstelle Metadaten", feedData);
             this.feedUrls.set(key, feedData);
             return {} as Feed;
         }
     };
 
     protected getNewsFeed = (url: string, key: string, period: number, withStatistic: boolean): Subscription => {
-        this.logDebug("fetch begin für " + url + " mit key " + key);
+       this.LOG.logDebug("fetch begin für " + url + " mit key " + key);
         const feed$: Observable<AxiosResponse> = timer(0, period).pipe(
             tap(() => console.log("Neue Abfrage von " + url)),
             switchMap(() => from(axios.get(url)).pipe(catchError(() => EMPTY))),
@@ -81,7 +68,7 @@ class Feeder {
         return feed$.subscribe(
             (feedResponse: AxiosResponse) => {
                 if (feedResponse.status != 200) {
-                    this.logError(new Error(`status code ${feedResponse.status}`));
+                   this.LOG.logError(new Error(`status code ${feedResponse.status}`));
                     return;
                 }
                 let parser = new FeedMe(true);
@@ -89,22 +76,22 @@ class Feeder {
                 const feed = parser.done() as Feed;
                 this.speichereResponsedaten(key, feed);
             }, (error) => {
-                this.logError(new Error(`Response failed with: ${error}`));
+               this.LOG.logError(new Error(`Response failed with: ${error}`));
             }, () => {
-                this.logDebug("Feed complete for " + url + "(" + key + ")");
+               this.LOG.logDebug("Feed complete for " + url + "(" + key + ")");
             }
         );
     };
 
     protected speichereResponsedaten(key: string, feed: Feed) {
-        this.logDebug("Suche Metadaten zur Ablage der Responsedaten.");
+       this.LOG.logDebug("Suche Metadaten zur Ablage der Responsedaten.");
         const metaData: UrlMetaData = this.feedUrls.get(key) as UrlMetaData;
         if (metaData) {
-            this.logDebug("Feed Data: " + JSON.stringify(feed));
-            this.logDebug("Data received for : " + metaData.url);
+           this.LOG.logDebug("Feed Data: " + JSON.stringify(feed));
+           this.LOG.logDebug("Data received for : " + metaData.url);
             metaData.data = feed;
         } else {
-            this.logDebug("Keine Metadaten zur Anfrage gefunden");
+           this.LOG.logDebug("Keine Metadaten zur Anfrage gefunden");
         }
     }
 }
