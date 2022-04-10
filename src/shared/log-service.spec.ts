234fe2a61b466ce3efd7c86be06c9bd30a201eaa
huluvu424242 <huluvu424242@gmail.com>
2022-04-10 14:04:43 +0200

    asserts ergänzt
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/shared/log-service.spec.ts
+++ src/shared/log-service.spec.ts
@@ -1,25 +1,37 @@
 import {logService} from "./log-service";
 import {spy} from "sinon";
-import 'mocha';
+import {afterEach, describe, it} from 'mocha';
 import {expect, should, use} from 'chai';
 
 const sinonChai = require("sinon-chai");
 should();
 use(sinonChai);
 
-describe('Hello function', () => {
+describe('LogService', () => {
 
-    it('should return hello world', () => {
+    const logSpy = spy(console, 'log');
+    const infoSpy = spy(console, 'info');
+    const warningSpy = spy(console, 'warn');
+    const errorSpy = spy(console, 'error');
+    const debugSpy = spy(console, 'debug');
 
-        let mySpy = spy(console, 'log');
-
-        logService.logMessage("hallo");
-
-        expect(mySpy.calledWith("hallo")).to.be.ok;
+    afterEach( 'Reset: Mock and Spy', ()=> {
 
         // restore the original function
-        mySpy.restore();
+        logSpy.restore();
+        infoSpy.restore();
+        warningSpy.restore();
+        errorSpy.restore();
+        debugSpy.restore();
+    });
 
+
+    it('logMessage logt über console.log', () => {
+        const testParameter = {"error":3};
+
+        logService.logMessage("hallo",testParameter);
+
+        expect(logSpy.calledWithExactly("hallo", [[testParameter]])).to.be.ok;
     });
 
 });
