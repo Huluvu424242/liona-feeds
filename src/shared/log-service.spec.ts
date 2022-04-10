a508c06228fd748cb420d8b33102f3e5fc9c1beb
huluvu424242 <huluvu424242@gmail.com>
2022-04-10 12:16:25 +0200

    working test und build config with correct asserts
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/shared/log-service.spec.ts
+++ src/shared/log-service.spec.ts
@@ -1,11 +1,25 @@
 import {logService} from "./log-service";
-import { expect } from 'chai';
+import {spy} from "sinon";
 import 'mocha';
+import {expect, should, use} from 'chai';
+
+const sinonChai = require("sinon-chai");
+should();
+use(sinonChai);
 
 describe('Hello function', () => {
 
     it('should return hello world', () => {
-        expect(logService).to.equal('Hello world!');
+
+        let mySpy = spy(console, 'log');
+
+        logService.logMessage("hallo");
+
+        expect(mySpy.calledWith("hallo")).to.be.ok;
+
+        // restore the original function
+        mySpy.restore();
+
     });
 
 });
