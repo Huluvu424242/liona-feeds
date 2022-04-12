fae1a86912b634cbadd1b52a89108b1ecbae521f
huluvu424242 <huluvu424242@gmail.com>
2022-04-12 13:13:43 +0200

    refactoring cleaner
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/feeds/cleaner.ts
+++ src/feeds/cleaner.ts
@@ -1,5 +1,4 @@
-import {from, Subscription, timer} from "rxjs";
-import {filter, switchMap} from "rxjs/operators";
+import {filter, from, Observable, Subscription, switchMap, timer} from "rxjs";
 import {logService} from "../shared/log-service";
 import {FeedMetadata} from "./metadata";
 
@@ -10,37 +9,56 @@ const TIMEOUT_DELTA: number = 60000 * 60; // alle 60 Minuten
 export class Cleaner {
 
     protected feedMap: Map<string, FeedMetadata>;
+    protected jobPeriod: number;
+    protected timeoutDelta: number;
     protected subscription: Subscription;
 
-    public constructor(feedMap: Map<string, FeedMetadata>) {
+    public constructor(feedMap: Map<string, FeedMetadata>, jobPeriod: number = JOB_PERIOD, timeoutDelta: number = TIMEOUT_DELTA) {
         this.feedMap = feedMap;
-        this.subscription = this.startCleanup();
-    }
-
-    protected startCleanup(): Subscription {
-        const cleanUpJob$ = timer(0, JOB_PERIOD).pipe(
-            switchMap(
-                () => from(this.feedMap.keys())
-            ),
-
-            filter((key: string) => {
-                const feedMetadata: FeedMetadata = this.feedMap.get(key) as FeedMetadata
-                if (!feedMetadata) return true; // -> entfernen
-                const current: number = Date.now();
-                if ((current - feedMetadata.lastRequested.getTime()) > TIMEOUT_DELTA) {
-                    feedMetadata.subscription.unsubscribe();
-                    return true;
-                } else {
-                    return false;
-                }
-            }),
-        );
-        return cleanUpJob$.subscribe(
-            (key: string) => {
-                this.feedMap.delete(key);
-                logService.debugMessage("Subscription für Key " + key + " beendet.");
+        this.jobPeriod = jobPeriod;
+        this.timeoutDelta = timeoutDelta;
+        this.subscription = this.feedKeysToRemove$().subscribe({
+                next: (key: string) => this.removeAndCleanUpKey(key),
+                error: (error: any) => logService.errorMessage(error),
+                complete: () => logService.infoMessage("Subscription of cleanUpJob finished")
             }
         );
     }
 
+    feedKeysToRemove$(): Observable<string> {
+        return this.keylistOfFeeds$().pipe(
+            filter(
+                (key: string) => this.tooFewRequested(key)
+            ),
+        );
+    }
+
+    keylistOfFeeds$(): Observable<string> {
+        return timer(0, this.jobPeriod).pipe(
+            switchMap(
+                () => from(this.feedMap.keys())
+            ),
+        );
+    }
+
+    tooFewRequested(key: string): boolean {
+        const feedMetadata: FeedMetadata = this.feedMap.get(key) as FeedMetadata
+        if (!feedMetadata) return true; // -> entfernen
+        const current: number = Date.now();
+        return (current - feedMetadata.lastRequested.getTime()) > this.timeoutDelta;
+    }
+
+    unsubscribeFeedInMetadata(key: string) {
+        const feedMetadata: FeedMetadata = this.feedMap.get(key) as FeedMetadata;
+        if (feedMetadata) {
+            feedMetadata.subscription.unsubscribe();
+            logService.debugMessage("Subscription für feed " + key + " beendet.");
+        }
+    }
+
+    removeAndCleanUpKey(key: string) {
+        this.unsubscribeFeedInMetadata(key);
+        this.feedMap.delete(key);
+        logService.debugMessage("Feed " + key + " aus Feedliste entfernt.");
+    }
 }
